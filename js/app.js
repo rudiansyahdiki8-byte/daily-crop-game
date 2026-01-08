c4b0dac93b05df9055e14801ced2e6ba2a2c320e
rudiansyahdiki8-byte <rudiansyahdiki8@gmail.com>
2026-01-08 23:16:19 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,14 +9,20 @@ const App = {
         "<h3>Please open this app via Telegram</h3>";
       return;
     }
-
+    
     fetch("ui/ui.html")
       .then(res => res.text())
       .then(html => {
         document.getElementById("app").innerHTML = html;
         console.log("UI loaded successfully");
+    
+        // INIT MODULES AFTER UI READY
+        if (typeof initUI === "function") initUI();
+        if (typeof renderFarm === "function") renderFarm();
       });
+
   }
 };
 
 document.addEventListener("DOMContentLoaded", App.init);
+
