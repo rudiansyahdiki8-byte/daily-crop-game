b1bb5395f8ca5aa006eea58d41d75247e96fa576
rudiansyahdiki8-byte <rudiansyahdiki8@gmail.com>
2026-01-15 00:44:04 +0700

    Update market.js

--- js/market.js
+++ js/market.js
@@ -437,7 +437,7 @@ renderSellInventory() {
         UIEngine.showRewardPopup("PRICE BOOSTER", "Watch Ad for +20% sell price?", async () => {
             
             // PANGGIL IKLAN
-            AdsManager.showAd("Activate Booster", 3, async () => {
+            AdsManager.showAd("Activate Booster", async () => {
                 GameState.user.adBoosterCooldown = Date.now() + 86400000; 
                 await GameState.save();
                 this.checkBoosterStatus();
@@ -477,3 +477,4 @@ renderSellInventory() {
 
 
 window.MarketSystem = MarketSystem;
+
