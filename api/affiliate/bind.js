2d41c8fba0145ef78a0ad5be9cd0992e320632d8
rudiansyahdiki8-byte <rudiansyahdiki8@gmail.com>
2026-01-18 08:18:53 +0700

    Update bind.js

--- api/affiliate/bind.js
+++ api/affiliate/bind.js
@@ -1,4 +1,4 @@
-import { db } from '../utils/firebase';
+import { db } from '../utils/firebase.js';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
